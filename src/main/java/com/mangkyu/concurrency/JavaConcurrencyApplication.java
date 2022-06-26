78d25d5911f4057205cb667fd7ac4c46db1db365
MangKyu <whalsrb1226@gmail.com>
2022-06-26 14:23:50 +0900

    Initial commit

---
+++ src/main/java/com/mangkyu/concurrency/JavaConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.mangkyu.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaConcurrencyApplication.class, args);
+    }
+
+}
