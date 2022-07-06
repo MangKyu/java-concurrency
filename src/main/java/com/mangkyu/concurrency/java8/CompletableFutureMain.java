6fb08da55a1a2168eda0560066bd27e51c0f1290
MangKyu <whalsrb1226@gmail.com>
2022-07-06 22:53:35 +0900

    Add CompletableFutureMain

---
+++ src/main/java/com/mangkyu/concurrency/java8/CompletableFutureMain.java
@@ -0,0 +1,21 @@
+package com.mangkyu.concurrency.java8;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CompletableFutureMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // More codes on Test code
+        CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
+            return "Thread: " + Thread.currentThread().getName();
+        }).thenAccept(s -> {
+            System.out.println(s.toUpperCase());
+        });
+
+        future.get();
+    }
+
+}
