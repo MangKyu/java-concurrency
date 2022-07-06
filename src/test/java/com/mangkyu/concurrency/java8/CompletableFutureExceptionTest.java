9b7dca09ece343b89fef05392f2605f36b479f61
MangKyu <whalsrb1226@gmail.com>
2022-07-06 22:48:28 +0900

    Refactor CompletableFutureExceptionTest

--- src/test/java/com/mangkyu/concurrency/java8/CompletableFutureExceptionTest.java
+++ src/test/java/com/mangkyu/concurrency/java8/CompletableFutureExceptionTest.java
@@ -1,16 +1,16 @@
 package com.mangkyu.concurrency.java8;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 class CompletableFutureExceptionTest {
 
-    @Test
-    void exceptionally_throw() throws ExecutionException, InterruptedException {
-        boolean doThrow = true;
-
+    @ParameterizedTest
+    @ValueSource(booleans =  {true, false})
+    void exceptionally(boolean doThrow) throws ExecutionException, InterruptedException {
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
             if (doThrow) {
                 throw new IllegalArgumentException("Invalid Argument");
@@ -24,46 +24,9 @@ class CompletableFutureExceptionTest {
         System.out.println(future.get());
     }
 
-    @Test
-    void exceptionally_notThrow() throws ExecutionException, InterruptedException {
-        boolean doThrow = false;
-
-        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
-            if (doThrow) {
-                throw new IllegalArgumentException("Invalid Argument");
-            }
-
-            return "Thread: " + Thread.currentThread().getName();
-        }).exceptionally(e -> {
-            return e.getMessage();
-        });
-
-        System.out.println(future.get());
-    }
-
-    @Test
-    void handle_throw() throws ExecutionException, InterruptedException {
-        boolean doThrow = true;
-
-        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
-            if (doThrow) {
-                throw new IllegalArgumentException("Invalid Argument");
-            }
-
-            return "Thread: " + Thread.currentThread().getName();
-        }).handle((result, e) -> {
-            return e == null
-                    ? result
-                    : e.getMessage();
-        });
-
-        System.out.println(future.get());
-    }
-
-    @Test
-    void handle_notThrow() throws ExecutionException, InterruptedException {
-        boolean doThrow = false;
-
+    @ParameterizedTest
+    @ValueSource(booleans =  {true, false})
+    void handle(boolean doThrow) throws ExecutionException, InterruptedException {
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
             if (doThrow) {
                 throw new IllegalArgumentException("Invalid Argument");
